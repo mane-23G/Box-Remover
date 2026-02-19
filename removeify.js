91f9f55ffb4e51ef29f098bfb887ba6cad7fb04f
mane <bobfisherman30@gmail.com>
2026-02-19 16:51:52 -0500

    The extension for removing the box

---
+++ removeify.js
@@ -0,0 +1,13 @@
+console.log("Content script is running");
+
+const observer = new MutationObserver(() => {
+    const boxtoremove = document.querySelectorAll(".relative.shadow-lg div")
+    const wordbox = boxtoremove[2]
+    const yellowbox = boxtoremove[3]
+    wordbox.remove()
+    yellowbox.remove()
+})
+
+
+// Observe changes in the DOM
+observer.observe(document.body, { childList: true, subtree: true });
